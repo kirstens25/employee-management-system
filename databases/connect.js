4c16a9cccdf70b0c310fc9240ad67ff3ba5960ea
Kirsten Segavac <kirstensegavac@gmail.com>
2023-02-06 21:09:53 +0800

    added mysql, dev dep

---
+++ databases/connect.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2/promise');
+
+require('dotenv').config();
+
+// expose a mysql db connection
+async function connect(){
+
+    return await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: "employees_cms",
+    });
+}
+
+module.exports = {
+    connect
+}
